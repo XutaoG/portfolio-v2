f66bcd764b79dd2bcc8c49a449dd0da2d1607843
Xutao Gao <xu611268@ucf.edu>
2026-06-09 16:05:39 -0400

    Remove redundant onMouseEnter and onMouseLeave and replace hover interaction code with tailwind group

--- client/src/components/Common/Button.tsx
+++ client/src/components/Common/Button.tsx
@@ -1,4 +1,4 @@
-import { useState, type ButtonHTMLAttributes } from "react";
+import { type ButtonHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -7,14 +7,10 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button = ({ className, children, disabled, ...rest }: ButtonProps) => {
-	const [isMouseHover, setIsMouseHover] = useState(false);
-
 	return (
 		<button
 			className={`relative transiton-transform duration-300 disabled:cursor-not-allowed 
-			${disabled || "active:translate-y-1"} ${className}`}
-			onMouseEnter={() => setIsMouseHover(true)}
-			onMouseLeave={() => setIsMouseHover(false)}
+			group ${disabled || "active:translate-y-1"} ${className}`}
 			disabled={disabled}
 			{...rest}
 		>
@@ -22,17 +18,17 @@ export const Button = ({ className, children, disabled, ...rest }: ButtonProps) 
 			<div
 				className={twMerge(`px-6 py-3 gap-3 border border-primary rounded-sm bg-base
 				font-semibold flex items-center relative overflow-hidden transition-transform duration-300
-				z-10 ${disabled && "border-primary/40"} ${isMouseHover && "-translate-1.5"}`)}
+				z-10 group-hover:-translate-1.5 ${disabled && "border-primary/40"}`)}
 			>
 				<div
 					className={twMerge(
 						`absolute top-1/2 left-1/2 -translate-1/2 bg-primary rounded-full w-0 aspect-square 
-						transition-[width] duration-300 ${isMouseHover && "w-[120%]"}`,
+						transition-[width] duration-300 group-hover:w-[120%]`,
 					)}
 				/>
 				<span
 					className={twMerge(`z-20 font-medium text-primary 
-					transition-colors duration-300 ${isMouseHover && "text-black"}`)}
+					transition-colors duration-300 group-hover:text-black`)}
 				>
 					$
 				</span>
--- client/src/components/HomePage/QuickSocial.tsx
+++ client/src/components/HomePage/QuickSocial.tsx
@@ -3,7 +3,6 @@ import { useRef, useState } from "react";
 import { Link } from "react-router";
 
 export const QuickSocial = () => {
-	const [isHoverEmailButton, setIsHoverEmailButton] = useState(false);
 	const [hasCopidEmail, setHasCopiedEmail] = useState(false);
 	const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
@@ -22,7 +21,7 @@ export const QuickSocial = () => {
 		"rounded-sm border border-primary/40 flex justify-center items-center p-3 hover:-translate-y-1.5 hover:border-primary/60 transition-all duration-300 relative overflow-hidden group";
 
 	const gradientElement = (
-		<div className="z-0 absolute bg-primary/40 size-12 -bottom-6 -right-6 rounded-full blur-xl group-hover:bg-primary/80" />
+		<div className="z-0 absolute bg-primary/40 size-12 -bottom-6 -left-6 rounded-full blur-xl group-hover:bg-primary/80" />
 	);
 
 	return (
@@ -37,18 +36,17 @@ export const QuickSocial = () => {
 			</Link>
 			<div
 				className="rounded-sm border border-primary/40 flex justify-center items-center 
-				relative overflow-hidden group hover:border-primary/60 transition-all duration-300"
-				onMouseEnter={() => setIsHoverEmailButton(true)}
-				onMouseLeave={() => setIsHoverEmailButton(false)}
+				relative overflow-hidden group hover:border-primary/60 transition-all duration-300 group"
 			>
 				{gradientElement}
 				<div className="p-3">
 					<EnvelopeIcon size={24} weight="bold" />
 				</div>
 				<div
-					className={`h-full flex justify-end items-center overflow-hidden gap-2
-						transition-[width] duration-300 ${isHoverEmailButton ? "w-64 pr-3" : "w-0"}`}
+					className={`h-full flex justify-end items-center overflow-hidden gap-2 relative
+						transition-[width] duration-300 w-0 group-hover:w-64 group-hover:pr-3`}
 				>
+					<div className="z-0 absolute bg-primary/40 size-12 -bottom-6 -right-6 rounded-full blur-xl group-hover:bg-primary/80" />
 					<p>{myEmail}</p>
 					<button onClick={copyEmail}>
 						{hasCopidEmail ? (
