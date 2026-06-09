7bdf8e1c018143a6024868f28f3b21a27860f8d4
Xutao Gao <xu611268@ucf.edu>
2026-06-09 01:35:16 -0400

    Add button hover animation

--- client/src/components/Common/Button.tsx
+++ client/src/components/Common/Button.tsx
@@ -1,4 +1,4 @@
-import type { ButtonHTMLAttributes } from "react";
+import { useState, type ButtonHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -7,13 +7,35 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const Button = ({ className, children, ...rest }: ButtonProps) => {
+	const [isMouseHover, setIsMouseHover] = useState(false);
+
 	return (
 		<button
-			className={twMerge(`px-6 py-4 gap-3 border-2 border-primary rounded-sm 
-				font-semibold flex items-center ${className}`)}
+			className={`relative ${className}`}
+			onMouseEnter={() => setIsMouseHover(true)}
+			onMouseLeave={() => setIsMouseHover(false)}
 			{...rest}
 		>
-			<span className="text-primary">$</span> {children}
+			<div className="absolute inset-0 rounded-sm bg-primary/20" />
+			<div
+				className={twMerge(`px-6 py-3 gap-3 border border-primary rounded-sm bg-base
+				font-semibold flex items-center relative overflow-hidden transition-transform duration-300
+				z-10 ${isMouseHover && "-translate-1.5"}`)}
+			>
+				<div
+					className={twMerge(
+						`absolute top-1/2 left-1/2 -translate-1/2 bg-primary rounded-full w-0 aspect-square 
+						transition-[width] duration-300 ${isMouseHover && "w-[120%]"}`,
+					)}
+				/>
+				<span
+					className={twMerge(`z-20 font-medium text-primary 
+					transition-colors duration-300 ${isMouseHover && "text-black"}`)}
+				>
+					$
+				</span>
+				<span className="z-20">{children}</span>
+			</div>
 		</button>
 	);
 };
